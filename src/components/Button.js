f86193622f996877b569faff19f586cb3478f61f
Michael <msiller455@gmail.com>
2020-07-16 19:37:03 -0700

    configures button components

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ButtonInline = ({onClick, type = 'button', children}) =>
+    <button
+      type={type}
+      className="button-inline"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+
+const Button = ({onClick, className, type = 'button', children}) =>
+    <button
+      type={type}
+      className={className}
+      onClick={onClick}
+    >
+      {children}
+    </button>
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Story.css'
 
-const Story = ({ story, columns }) => {
+const Story = ({ story, columns, onArchive }) => {
     const {
         title,
         url,
         author,
         num_comments,
         points,
+        objectID,
     } = story
 
     return (
@@ -25,9 +26,14 @@ const Story = ({ story, columns }) => {
                 {points}
             </span>
             <span style={{ width: columns.archive.width }}>
+                <ButtonInline onClick={() => onArchive(objectID)}>
+                    Archive
+                </ButtonInline>
             </span>
         </div>
     )
 }
+
+
 
 export default Story
